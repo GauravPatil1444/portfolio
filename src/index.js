99aa4ab8a339682a9b683f2ce1749d9f44b33a9d
Gaurav Patil <patilgauravajit@gmail.com>
2024-07-26 19:24:31 +0530

    update

---
+++ src/index.js
@@ -0,0 +1,7 @@
+let menuclick = ()=>{   
+   let menuimg =  document.getElementById('menuicon').src;
+   menuimg == "/assets/menu.png"?"/assets/cross.png":"/assets/menu.png";
+}
+let downloadcv = ()=>{
+   window.location.href = "https://drive.google.com/file/d/1Yl0wPQdY6QWYO5cyLNrxXbhd52uRwH2G/view?usp=drivesdk";
+}
